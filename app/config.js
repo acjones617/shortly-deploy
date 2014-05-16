b04cc1d1dedd890df0bfb87c517fb0fbc19e4674
Hack Reactor Students <accounts@hackreactor.com>
2014-05-15 20:32:03 -0700

    Configs mongo schema

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 // var Bookshelf = require('bookshelf');
-var Mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var crypto = require('crypto');
@@ -7,98 +7,19 @@ var crypto = require('crypto');
 
 console.log('config file running');
 
-exports.db = Mongoose.connection;
-Mongoose.connect('mongodb://localhost/test');
+var mongodb = process.env.MONGO_URI || 'mongodb://localhost/test';
 
-exports.db.on('error', console.error);
-
-//console.log(exports, 'this is running');
-exports.urlsSchema = new Mongoose.Schema({
-  url: String,
-  base_url: String,
-  code: String,
-  title: String,
-  visits: {type: Number, default: 0},
-  date: { type: Date, default: Date.now }
-});
-//created usersSchema
-exports.usersSchema = new Mongoose.Schema({
-  username: String,
-  password: String,
-  date: { type: Date, default: Date.now },
-});
-
-//created methods
-exports.urlsSchema.methods.setCode = function(cb) {
-  var shasum = crypto.createHash('sha1');
-  shasum.update(this.url);
-  this.code = shasum.digest('hex').slice(0,5);
-  cb();
-};
-
-exports.usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
-  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
-    callback(isMatch);
-  });
-};
-
-exports.usersSchema.methods.hashPassword = function(){
-  var cipher = Promise.promisify(bcrypt.hash);
-  return cipher(this.password, null, null).bind(this)
-    .then(function(hash) {
-      this.password = hash;
-    });
-};
-
-  // exports.Links = exports.db.model('Links', urlsSchema);
-  // exports.Users = exports.db.model('Users', usersSchema);
-
-exports.db.once('open', function() {
-  //created urlSchema
-  //
-});
+//exports.db = mongoose.connection;
+//exports.db = mongoose;
 
 
-//module.exports = db;
+mongoose.connect(mongodb);
 
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
+mongoose.connection.on('error', console.error);
 
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
 
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+// exports.db.once('open', function() {
+//   console.log('')
+// }); //  db.once not neede
 
+module.exports = mongoose;
