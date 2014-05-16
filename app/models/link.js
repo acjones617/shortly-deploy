2ee4f86ce42ec46c162c4266ab7c8954d55b321f
Hack Reactor Students <accounts@hackreactor.com>
2014-05-15 20:34:38 -0700

    builds schema and crypto functionality

--- app/models/link.js
+++ app/models/link.js
@@ -1,7 +1,25 @@
 var mongoose = require('../config');
 var crypto = require('crypto');
 
-exports.Links = mongoose.db.model('Links', mongoose.urlsSchema);
+var urlsSchema = new mongoose.Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: {type: Number, default: 0},
+  date: { type: Date, default: Date.now }
+});
+
+urlsSchema.pre('save', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0,5);
+  next();
+});
+
+//add schema, add methods
+module.exports = mongoose.model('Links', urlsSchema);
+
 
 // exports.Links = exports.db.model('Links', urlsSchema);
 
